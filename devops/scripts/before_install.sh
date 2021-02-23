5dcb1ab31716d239d6bd3075ec0ee1f9fe50417d
EllaiSys <admin@ellaisys.com>
2021-02-23 18:29:16 +0530

    update devops scripts

--- devops/scripts/before_install.sh
+++ devops/scripts/before_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Deleting LICENSE.md file"
-rm -f /ellaisys/solutions/crmomni/$DEPLOYMENT_GROUP_NAME/LICENSE.md
+# rm -f /ellaisys/solutions/crmomni/$DEPLOYMENT_GROUP_NAME/LICENSE.md
 echo "Deleted LICENSE.md file"
