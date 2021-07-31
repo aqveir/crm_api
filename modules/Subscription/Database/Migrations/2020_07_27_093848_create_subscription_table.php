9d734863e5ea983ce00961c577253c8b54e120fd
EllaiSys <admin@ellaisys.com>
2021-07-31 23:03:37 +0530

    WIP: subscription migration scripts

--- modules/Subscription/Database/Migrations/2020_07_27_093848_create_subscription_table.php
+++ modules/Subscription/Database/Migrations/2020_07_27_093848_create_subscription_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSubscriptionplanTable extends Migration
+class CreateSubscriptionTable extends Migration
 {
     /**
      * Run the migrations.
@@ -35,6 +35,22 @@ class CreateSubscriptionplanTable extends Migration
 
             $table->timestamps();
             $table->softDeletes();
+
+
+            // FROM Cashier migrations
+            //
+            // $table->bigIncrements('id');
+            // $table->unsignedBigInteger('user_id');
+            // $table->string('name');
+            // $table->string('stripe_id');
+            // $table->string('stripe_status');
+            // $table->string('stripe_price')->nullable();
+            // $table->integer('quantity')->nullable();
+            // $table->timestamp('trial_ends_at')->nullable();
+            // $table->timestamp('ends_at')->nullable();
+            // $table->timestamps();
+
+            // $table->index(['user_id', 'stripe_status']);
         });
     }
 
---
+++ modules/Subscription/Database/Migrations/2021_07_28_202057_create_subscription_items_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubscriptionItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(config('crmomni-migration.table_name.subscription.items'), function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('subscription_id');
+
+            $table->string('stripe_id')->index();
+            $table->string('stripe_product');
+            $table->string('stripe_price');
+            
+            $table->integer('quantity')->nullable();
+            $table->timestamps();
+
+            $table->unique(['subscription_id', 'stripe_price']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists(config('crmomni-migration.table_name.subscription.items'));
+    }
+}
