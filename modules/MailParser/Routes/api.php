20e17edd5dc5d5823ed842d7585558be92f46f13
EllaiSys <admin@ellaisys.com>
2022-02-01 01:47:28 +0530

    fix: setup api endpoints

--- modules/MailParser/Routes/api.php
+++ modules/MailParser/Routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use Modules\Boilerplate\Routing\Router;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +12,29 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+$api = app(Router::class);
 
-Route::middleware('auth:api')->get('/mailparser', function (Request $request) {
-    return $request->user();
+$api->version('v1', [
+        'prefix' => 'api',
+        'middleware' => ['api'],
+        'namespace' => 'Modules\MailParser\Http\Controllers',
+        'domain' => config('crmomni.settings.domain')
+    ], function (Router $api) {
+
+    // Unauthenticated Endpoints for Mail Parser
+    $api->group(['prefix' => 'mailparser', 'middleware' => ['guest']], function(Router $api) {
+
+        // Exotel Endpoints
+        $api->group(['prefix' => 'exotel'], function(Router $api) {
+            //Exotels Calls
+            $api->any('callback','Exotel\\VoiceController@callback');
+            $api->any('passthru','Exotel\\VoiceController@passthru');
+
+            //Exotels SMS
+            //$api->any('sms/callback','Exotel\\SmsController@callback');
+
+            //TODO: Delete this
+            $api->any('call/customer','Exotel\\VoiceController@test');
+        });
+    });
 });
