9403acfbd9fb81008c6fd11b265e13f0324315f1
EllaiSys <admin@ellaisys.com>
2022-02-01 01:34:59 +0530

    feature: mail parser

---
+++ modules/MailParser/Database/Seeders/MailParserDatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\MailParser\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class MailParserDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        // $this->call("OthersTableSeeder");
+    }
+}
