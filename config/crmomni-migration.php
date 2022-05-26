28fc98d641df205ab9049eb52d3793640ac1e440
Amit <amit.dhongde@gmail.com>
2022-05-26 23:41:33 +0530

    chore: upgrade laravel and libs

--- config/crmomni-migration.php
+++ config/crmomni-migration.php
@@ -32,6 +32,7 @@ return [
         //Subscription Entities
         'subscription' => [
             'main' => env('TABLE_NAME_PREFIX', '') . 'subscriptions',
+            'plan' => env('TABLE_NAME_PREFIX', '') . 'subscription_plans',
             'items' => env('TABLE_NAME_PREFIX', '') . 'subscription_items',
             'organizations' => env('TABLE_NAME_PREFIX', '') . 'organization_subscriptions',
         ],
