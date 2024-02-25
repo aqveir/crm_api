e9607139cc96d1d36f5387fc79acb5f32b223552
Product Team [aQveir] <support@aqveir.com>
2024-02-25 15:11:19 +0530

    chore: fix for the console url checks

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,28 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+
+// Route for the web console
+Route::domain(config('aqveir.settings.domain'))->group(function() {
+	// Default route
+	Route::get('/', function(Request $request, string $subdomain) {
+		$subdomainValid = true;
+
+		//Check if the subdomains is part of restricted list
+		$subdomainsRestricted = config('aqveir.settings.restricted_subdomains');
+		$subdomainValid = (in_array($subdomain, $subdomainsRestricted))?false:true;
+
+		//Get the backend URI
+		$backendUri = config('aqveir.settings.backend_uri');
+
+		//Route to the valid subdomain 
+		if ($subdomainValid) {
+			return redirect($backendUri);
+		} else {
+			return redirect($backendUri . config('aqveir.settings.frontend_error_uri'));
+		} //End if
+	});
+});
 
 // Reset Password Link For CRM Users
 Route::get('reset/{token}', ['as' => 'aqveir.user.password.reset', function(string $token, Request $request) {
