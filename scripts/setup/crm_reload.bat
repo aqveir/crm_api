fa3bd47c8e2939264f8e6e82ff120b0dac884223
EllaiSys <admin@ellaisys.com>
2026-01-15 13:06:26 +0530

    fix: resolve route issue in localhost

--- scripts/setup/crm_reload.bat
+++ scripts/setup/crm_reload.bat
@@ -7,7 +7,12 @@ php artisan cache:clear
 php artisan config:clear
 php artisan route:clear
 php artisan view:clear
+php artisan optimize:clear
 echo "Cache cleared."
+
+echo "Caching API routes..."
+php artisan api:cache
+echo "API routes cached."
 
 echo "Generating Laravel App Key..."
 php artisan key:generate --force --quiet
@@ -17,10 +22,15 @@ echo "Generating JWT Secret..."
 php artisan jwt:secret --force --quiet
 echo "Generated JWT Secret Key"
 
+exit /b 0
+
+:call_migrations
 echo "Creating DB..."
 php artisan migrate:refresh
 echo "DB created."
+exit /b 0
 
+:create_seeding
 echo "Creating meta and dummy data ..."
 php artisan module:seed Core
 php artisan module:seed Contact
@@ -31,3 +41,4 @@ php artisan module:seed Account
 php artisan module:seed ServiceRequest
 rem php artisan module:seed Wallet
 echo "Data created..."
+exit /b 0
